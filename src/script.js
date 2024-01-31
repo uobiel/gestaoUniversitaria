d13420d1e2c98b2de18b1bc9d229db6f4e6041b0
uobiel <gabrielantunesabc@gmail.com>
2024-01-31 20:57:53 -0300

    site completo, faltando pequenos ajustes com CSS

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const sair = document.querySelector('#sair');
+const pessoas = document.querySelector('#pessoas');
+const professores = document.querySelector('#professores');
+const alunos = document.querySelector('#alunos');
+const fornecedores = document.querySelector('#fornecedores');
+
+sair.addEventListener('click', function(){
+    window.location.href = '../login/login.html'
+})
+
+pessoas.addEventListener('click', function(){
+    window.location.href = '../pessoas/pessoas.html'
+})
+
+professores.addEventListener('click', function(){
+    window.location.href = '../professores/professores.html'
+})
+
+alunos.addEventListener('click', function(){
+    window.location.href = '../alunos/alunos.html'
+})
+
+fornecedores.addEventListener('click', function(){
+    window.location.href = '../fornecedores/fornecedores.html'
+})
+
+function voltarHome(){
+    window.location.href = '../home/home.html'
+}
